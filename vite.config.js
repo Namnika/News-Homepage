02966bf1b48871920e646269068885233b973db4
Namnika <janbandhunamnika@gmail.com>
2023-10-22 18:26:46 +0530

    Fixed some changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
+import { resolve } from "path";
+import fs from "fs/promises";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+	esbuild: {
+		jsxFactory: "h",
+		jsxFragment: "Fragment",
+		loader: "jsx",
+		include: /src\/.*\.jsx?$/,
+		exclude: []
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [
+				{
+					name: "load-js-files-as-jsx",
+					setup(build) {
+						build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+							loader: "jsx",
+							contents: await fs.readFile(args.path, "utf8")
+						}));
+					}
+				}
+			]
+		}
+	},
+	include: ["esm-dep > cjs-dep"],
+	resolve: {
+		alias: {
+			// eslint-disable-next-line no-undef
+			"@": resolve(__dirname, "./src")
+		}
+	},
 	plugins: [react()]
-});
+}));
