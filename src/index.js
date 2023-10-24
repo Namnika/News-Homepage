4664c3bc7b25334aaa116c9fa0e5b286b211f23b
Namnika <janbandhunamnika@gmail.com>
2023-10-24 17:24:16 +0530

    Modified some files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -10,4 +11,17 @@ root.render(
 	<StrictMode>
 		<App />
 	</StrictMode>
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+>>>>>>> parent of 24b83e9 (Added .prettierrc .prettierignore files)
 );
