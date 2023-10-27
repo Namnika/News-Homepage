6eb8f76c9abdb7760f253c670efc611412e4e64a
Namnika <janbandhunamnika@gmail.com>
2023-10-27 16:16:37 +0530

    Added a block structure for main content

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,11 +5,22 @@ import "../styles/index.css";
 
 export default function Main() {
 	return (
-		<div className="bg-slate-50 overflow-hidden w-full text-center h-screen">
-			<div className="lg:py-24 lg:px-24 py-8 px-8">
-				<Navbar />
-				{/* <PopularNews />
-				<Trending /> */}
+		<div className="bg-slate-50 overflow-hidden lg:py-24 lg:px-36 py-8 px-8 w-full text-center h-screen">
+			{/* Navbar */}
+
+			<Navbar />
+
+			{/* Main content */}
+			<div className="big-block my-16 h-5/6 border-2 border-black">
+				<div >
+					{/* main news */}
+				</div>
+				<div>
+					{/* Trending block */}
+				</div>
+				<div>
+					{/* Popular block */}
+				</div>
 			</div>
 		</div>
 	);
