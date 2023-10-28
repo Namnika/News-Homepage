986ab97cafb219b3c563453bff0f5dbc1709e04b
Namnika <janbandhunamnika@gmail.com>
2023-10-28 23:01:52 +0530

    modified news with 3X1 grid structure

--- src/components/PopularNews.js
+++ src/components/PopularNews.js
@@ -1,45 +1,93 @@
-import "../styles/index.css"
-import GamingImg from '../assets/images/image-gaming-growth.jpg'
-import RetroImg from '../assets/images/image-retro-pcs.jpg'
-import topImg from '../assets/images/image-top-laptops.jpg'
-
+import "../styles/index.css";
+import GamingImg from "../assets/images/image-gaming-growth.jpg";
+import RetroImg from "../assets/images/image-retro-pcs.jpg";
+import topImg from "../assets/images/image-top-laptops.jpg";
 
 export const PopularData = [
 	{
-		id: '1',
-		headlinenum: '01',
+		id: "1",
+		headlinenum: "01",
 		imgURL: RetroImg,
-		title: 'Reviving Retro PCs',
-		desc: 'What happens when old PCs are given modern upgrades?'
+		headline: "Reviving Retro PCs",
+		desc: "What happens when old PCs are given modern upgrades?"
 	},
 	{
-		id: '2',
-		headlinenum: '02',
+		id: "2",
+		headlinenum: "02",
 		imgURL: topImg,
-		title: 'Top 10 Laptops of 2022',
-		desc: 'Our best picks for various needs and budgets'
+		headline: "Top 10 Laptops of 2022",
+		desc: "Our best picks for various needs and budgets"
 	},
 	{
-		id: '3',
-		headlinenum: '03',
+		id: "3",
+		headlinenum: "03",
 		imgURL: GamingImg,
-		title: 'The Growth of Gaming',
-		desc: 'How the pandemic has sparked fresh opportunities.'
+		headline: "The Growth of Gaming",
+		desc: "How the pandemic has sparked fresh opportunities."
 	}
-]
+];
 
 export default function PopularNews() {
-	return <div className=''>
-		<div className='flex flex-row '>
-			<img src={RetroImg} className="mr-7" width={120} alt='retro-img' />
-			<div className='flex-col w-56 text-left antialiased'>
-				<span className="text-4xl text-[color:hsl(233,8%,79%)] font-['Inter-Bold']">01</span>
-				<div className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] hover:text-[color:hsl(5,85%,63%)]">Reviving Retro PCs</div>
-				<p className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2">What happens when old PCs are given modern upgrades?</p>
-			</div>
-		</div>
-		<div>
+	return (
+		<>
+			<div className="grid lg:grid-cols-3 gap-7">
+				{PopularData.map((data, index) => {
+					return (
+						<>
+							<div className="flex">
+								<div>
+									<img
+										className="lg:w-[150px] lg:h-[170px] w-[150px] h-[170px]"
+										src={data.imgURL}
+										key={index}
+										width={150}
+										height={200}
+										alt="retro-img"
+									/>
+								</div>
+								<div className="text-left space-x-5 space-y-5 antialiased">
+									<h3
+										key={index}
+										className="text-4xl text-[color:hsl(233,8%,79%)] ml-5 font-['Inter-Bold']"
+									>
+										{data.headlinenum}
+									</h3>
 
-		</div>
-	</div>;
+									<h4
+										key={index}
+										className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] hover:text-[color:hsl(5,85%,63%)]"
+									>
+										{data.headline}
+									</h4>
+
+									<p
+										key={index}
+										className="text-base font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2 md:line-clamp-2 leading-8"
+									>
+										{data.desc}
+									</p>
+								</div>
+							</div>
+						</>
+					);
+				})}
+			</div>
+
+			{/* {PopularData.map((data, index) => {
+			return <>
+
+
+					<img src={data.imgURL} key={index} width={120} alt='retro-img' />
+									
+						<span key={index} className="text-4xl text-[color:hsl(233,8%,79%)] font-['Inter-Bold']">{data.headlinenum}</span>
+
+						<h4 key={index} className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] hover:text-[color:hsl(5,85%,63%)]">{data.headline}</h4>
+
+						<p key={index} className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-3 leading-7">{data.desc}</p>
+					
+				
+			</>
+		})} */}
+		</>
+	);
 }
