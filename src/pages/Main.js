854f9ab34d7445877b298f41d8e31eb4edd53d6c
Namnika <janbandhunamnika@gmail.com>
2023-10-28 23:06:29 +0530

    modified some changes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,17 +13,21 @@ export default function Main() {
 			{/* Main content */}
 			<div className=" mx-auto my-16">
 				<div className="big-block grid h-5/6 gap-y-8 lg:grid-cols-3 lg:gap-8 border-2 border-black">
-
 					<div className="bg-amber-200 col-span-2  row-span-2 ">
 						{/* main news */}
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
 					</div>
 
 					<div className=" bg-lime-300  row-span-2">
 						{/* Trending block */}
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
 					</div>
-
 				</div>
 				{/* Popular block */}
 				<div className="border-2 border-black my-8 relative ">
