6fdb19c9edbbe455ef691521efbdf5d850d48d95
Namnika <janbandhunamnika@gmail.com>
2023-11-01 23:52:31 +0530

    fixed grid structure for main and trending news

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,22 +6,46 @@ import Web3ImgMobile from "../assets/images/image-web-3-mobile.jpg";
 import Button from "../components/Button";
 import "../styles/index.css";
 
+
+/*
+<div class="... grid grid-flow-row-dense grid-cols-1 gap-4 md:grid-cols-3">
+
+// img
+<div class="col-span-2 bg-slate-400">01</div> 
+
+// content
+  <div class="col-span-2">
+	<div class="grid grid-cols-1 md:grid-cols-2">
+	  <h1>2nd block</h1>
+	  <div class="bg-teal-200">b</div>
+	</div>
+  </div>
+
+// trending
+  <div class="bg-yellow-500">03</div>
+</div>
+
+https://play.tailwindcss.com/BMc4TWEQlG
+*/
+
+
 export default function Main() {
 	return (
 		<div className="bg-[color:hsl(36,100%,99%)] overflow-y-scroll lg:py-24 lg:px-28 py-8 px-4 w-full text-center h-screen">
-			{/* Navbar */}
 			<Navbar />
 
 			{/* Main content */}
 			<div className=" mx-auto my-16">
-				<div className="big-block grid gap-y-8 lg:grid-cols-3 lg:gap-8 border-2 border-black">
-					{/* main news */}
 
-					<div className="relative col-span-2  row-span-2 ">
-						<div>
-							<img src={Web3ImgDesktop} alt="web3-img" />
-						</div>
-						<div className="text-start py-5 absolute leading-8 grid lg:grid-cols-2 gap-6">
+				<div className="grid grid-flow-row-dense grid-cols-1 gap-4 md:grid-cols-3 border-2 border-black">
+					{/* img news */}
+					<div className="col-span-2 ">
+						<img src={Web3ImgDesktop} alt="web3-img" />
+					</div>
+
+					{/* content */}
+					<div className="text-start py-5 leading-8 col-span-2">
+						<div className="grid grid-cols-1 md:grid-cols-2">
 							<h2 className="text-[color:hsl(240,100%,5%)] font-['Inter-ExtraBold']  text-6xl">
 								The Bright Future of Web 3.0?
 							</h2>
@@ -35,10 +59,13 @@ export default function Main() {
 							</div>
 						</div>
 					</div>
-					{/* Trending block */}
-					<div className=" bg-[color:hsl(240,100%,5%)] relative row-span-2">
+
+					{/* Trending */}
+					<div className=" bg-[color:hsl(240,100%,5%)] ">
 						<Trending />
 					</div>
+
+
 				</div>
 				{/* Popular block */}
 				<div className="my-8 relative ">
