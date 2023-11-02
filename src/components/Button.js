9a113fb6e4d2539498c1c928d817712b1c3d9f52
Namnika <janbandhunamnika@gmail.com>
2023-11-02 13:27:38 +0530

    Changed `my-8` to `mt-8` and `mb-0`

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "../styles/index.css";
 export default function Button({ children }) {
 	return (
 		<>
-			<button className="uppercase tracking-[0.4em] text-[color:hsl(36,100%,99%)] py-4 px-10 bg-[color:hsl(5,85%,63%)] my-8 rounded text-base">
+			<button className="uppercase tracking-[0.4em] text-[color:hsl(36,100%,99%)] py-4 px-10 bg-[color:hsl(5,85%,63%)] mt-8 mb-0 rounded text-base">
 				{children}
 			</button>
 		</>
