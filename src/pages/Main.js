98ceced9f2e17bcc8c07a81714aeaa793a8e60b7
Namnika <janbandhunamnika@gmail.com>
2023-11-03 20:07:48 +0530

    modified some changes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,7 +6,6 @@ import Web3ImgMobile from "../assets/images/image-web-3-mobile.jpg";
 import Button from "../components/Button";
 import "../styles/index.css";
 
-
 export default function Main() {
 	return (
 		<div className="bg-[color:hsl(36,100%,99%)] overflow-y-scroll lg:py-24 lg:px-28 py-8 px-4 w-full text-center h-screen">
@@ -15,7 +14,6 @@ export default function Main() {
 			{/* Main content */}
 			<div className=" mx-auto my-16">
 				<div className="grid grid-flow-row-dense grid-cols-1 lg:gap-5 gap-y-16 lg:grid-cols-3">
-
 					{/* content */}
 					<div className=" col-span-2">
 						<img src={Web3ImgDesktop} className="py-0" alt="web3-img" />
@@ -39,7 +37,6 @@ export default function Main() {
 					<div className=" bg-[color:hsl(240,100%,5%)] lg:col-span-1 col-span-2">
 						<Trending />
 					</div>
-
 				</div>
 				{/* Popular block */}
 				<div className="lg:my-8 my-16 relative ">
