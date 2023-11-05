283294272bff2c21be1c0256cb7f9ce6938f2b01
Namnika <janbandhunamnika@gmail.com>
2023-11-05 16:40:21 +0530

    added `antialiased` and `scroll-smooth`

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,7 +8,7 @@ import "../styles/index.css";
 
 export default function Main() {
 	return (
-		<div className="bg-[color:hsl(36,100%,99%)] overflow-y-scroll lg:py-24 lg:px-28 py-8 px-4 w-full text-center h-screen">
+		<div className="bg-[color:hsl(36,100%,99%)] overflow-y-scroll lg:py-24 lg:px-28 py-8 px-4 w-full text-center h-screen antialiased scroll-smooth">
 			<Navbar />
 
 			{/* Main content */}
