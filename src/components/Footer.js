4526e588a669a31bc742335f06f293ea5d23b7ba
Namnika <janbandhunamnika@gmail.com>
2023-11-06 10:54:48 +0530

    fixed some spaces by `lg:space-x4`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
 	return (
 		<>
-			<footer className="py-12 lg:py-24">
-				<div className="flex flex-col lg:flex-row-reverse w-full border-t-[0.7px] space-y-5 space-x-4 text-center border-[color:hsl(233,8%,79%)]">
+			<footer className="py-12 lg:py-24 border-t-[1px] border-[color:hsl(233,8%,79%)]">
+				<div className="flex flex-col lg:flex-row-reverse w-full  space-y-5 lg:space-x-4 text-center ">
 					<div className="lg:text-right">social links</div>
-					<div className="lg:text-start text-[13px] font-['Inter-Regular'] ml-auto text-[color:hsl(236,13%,42%)]">
+					<div className="lg:text-start text-[13px] font-['Inter-Regular'] lg:ml-auto text-[color:hsl(236,13%,42%)]">
 						&copy; 2023 by
 						<a
 							rel="noreferrer"
