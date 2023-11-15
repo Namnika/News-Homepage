9ece4e421d3f8adc1889ca9b0f40fc9dcf7b74a3
Namnika <janbandhunamnika@gmail.com>
2023-11-15 13:57:42 +0530

    fetching news using useFetch hook

--- src/pages/PopularNews.js
+++ src/pages/PopularNews.js
@@ -1,25 +1,10 @@
-import axios from "axios";
-import { useState, useEffect, useCallback } from "react";
+import { useRef } from "react";
+import { useFetch } from "../hooks/useFetch";
 import "../styles/index.css";
 
 export default function PopularNews() {
-	const [popularNews, setPopularNews] = useState([]);
-	const [error, setError] = useState({});
-	const baseurl = "https://newsapi.org/v2/top-headlines";
+	const isComponentMounted = useRef(true);
 	const apiKey = "3d993edcc0e34e28b84450d9f7c95e36";
-
-	const fetchData = useCallback(
-		async (url) => {
-			return await axios
-				.get(url)
-				.then((res) => setPopularNews(res.data.articles))
-				.catch((err) => {
-					setError(err.response.data);
-					console.log(error);
-				});
-		},
-		[error]
-	);
 
 	const countries = [
 		"ae",
@@ -54,55 +39,53 @@ export default function PopularNews() {
 	const country = Math.floor(
 		Math.random(countries.map((q) => q)) * countries.length
 	);
-
 	const ct = countries[country];
 
-	useEffect(() => {
-		const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-		try {
-			sleep(5000);
+	const { news, loading } = useFetch(
+		`https://newsapi.org/v2/top-headlines?country=${ct}&apiKey=${apiKey}`,
+		isComponentMounted,
+		[]
+	);
 
-			fetchData(`${baseurl}?country=${ct}&apiKey=${apiKey}`);
-		} catch (err) {
-			setError(err.response.data);
-		}
-	}, [fetchData, ct]);
-
-	const slicedPopularNews = popularNews.slice(0, 3);
+	const popularNews = news.slice(0, 3);
 
 	return (
 		<>
 			<div className="grid lg:grid-cols-3 lg:gap-7 gap-10">
-				{slicedPopularNews.map((i, index) => {
-					return (
-						<>
-							<div className="flex">
-								<div>
-									<img
-										className="lg:w-[150px] lg:h-[170px] w-[150px] h-[170px]"
-										src={i.urlToImage}
-										width={150}
-										height={200}
-										alt="retro-img"
-									/>
-								</div>
-								<div className="text-left space-x-5 space-y-5 antialiased leading-8">
-									<h3 className="text-4xl text-[color:hsl(233,8%,79%)] ml-5 font-['Inter-Bold']">
-										0{index + 1}
-									</h3>
+				{loading ? (
+					<div>loading data...</div>
+				) : (
+					popularNews.map((i, index) => {
+						return (
+							<>
+								<div className="flex">
+									<div>
+										<img
+											className="lg:w-[150px] lg:h-[170px] w-[150px] h-[170px]"
+											src={i.urlToImage}
+											width={150}
+											height={200}
+											alt="img"
+										/>
+									</div>
+									<div className="text-left space-x-5 space-y-5 antialiased leading-8">
+										<h3 className="text-4xl text-[color:hsl(233,8%,79%)] ml-5 font-['Inter-Bold']">
+											0{index + 1}
+										</h3>
 
-									<h4 className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] cursor-pointer hover:text-[color:hsl(5,85%,63%)]">
-										{i.title}
-									</h4>
+										<h4 className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] cursor-pointer hover:text-[color:hsl(5,85%,63%)]">
+											{`${i.title.slice(0, 25)}...`}
+										</h4>
 
-									<p className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2 md:line-clamp-2">
-										{i.description.slice(0, 170)} + ...
-									</p>
+										<p className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2 md:line-clamp-2">
+											{i.description}
+										</p>
+									</div>
 								</div>
-							</div>
-						</>
-					);
-				})}
+							</>
+						);
+					})
+				)}
 			</div>
 		</>
 	);
--- src/pages/TrendingNews.js
+++ src/pages/TrendingNews.js
@@ -1,25 +1,10 @@
-import axios from "axios";
-import { useState, useEffect, useCallback } from "react";
+import { useRef } from "react";
+import { useFetch } from "../hooks/useFetch";
 import "../styles/index.css";
 
 export default function TrendingNews() {
-	const [trendingNews, setTrendingNews] = useState([]);
-	const [error, setError] = useState({});
-	const baseurl = "https://newsapi.org/v2/top-headlines";
+	const isComponentMounted = useRef(true);
 	const apiKey = "3d993edcc0e34e28b84450d9f7c95e36";
-
-	const fetchData = useCallback(
-		async (url) => {
-			return await axios
-				.get(url)
-				.then((res) => setTrendingNews(res.data.articles))
-				.catch((err) => {
-					setError(err.response.data);
-					console.log(error);
-				});
-		},
-		[error]
-	);
 
 	const sources = [
 		"bbc-news",
@@ -35,21 +20,15 @@ export default function TrendingNews() {
 	const source = Math.floor(
 		Math.random(sources.map((s) => s)) * sources.length
 	);
-
 	const so = sources[source];
 
-	useEffect(() => {
-		const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-		try {
-			sleep(5000);
+	const { news, loading } = useFetch(
+		`https://newsapi.org/v2/top-headlines?sources=${so}&apiKey=${apiKey}`,
+		isComponentMounted,
+		[]
+	);
 
-			fetchData(`${baseurl}?sources=${so}&apiKey=${apiKey}`);
-		} catch (err) {
-			setError(err.response.data);
-		}
-	}, [fetchData, so]);
-
-	const slicedTrendingNews = trendingNews.slice(0, 3);
+	const trendingNews = news.slice(0, 3);
 
 	return (
 		<div className="px-4 relative antialiased text-start flex flex-col">
@@ -60,20 +39,24 @@ export default function TrendingNews() {
 			</div>
 
 			<div className="leading-8 pt-5 divide-y divide-[color:hsl(236,13%,42%)]">
-				{slicedTrendingNews.map((i, index) => {
-					return (
-						<>
-							<div key={index} className="py-5 space-y-2">
-								<h4 className="text-[color:hsl(36,100%,99%)] text-lg font-['Inter-Bold'] cursor-pointer hover:text-[color:hsl(35,77%,62%)]">
-									{i.title}
-								</h4>
-								<p className="text-[color:hsl(233,8%,79%)] text-[15px] font-['Inter-Regular']">
-									{i.description.slice(0, 160)} + ...
-								</p>
-							</div>
-						</>
-					);
-				})}
+				{loading ? (
+					<div>loading data...</div>
+				) : (
+					trendingNews.map((i, index) => {
+						return (
+							<>
+								<div key={index} className="py-5 space-y-2">
+									<h4 className="text-[color:hsl(36,100%,99%)] text-lg font-['Inter-Bold'] cursor-pointer hover:text-[color:hsl(35,77%,62%)]">
+										{i.title}
+									</h4>
+									<p className="text-[color:hsl(233,8%,79%)] text-[15px] font-['Inter-Regular']">
+										{`${i.description.slice(0, 170)}...`}
+									</p>
+								</div>
+							</>
+						);
+					})
+				)}
 			</div>
 		</div>
 	);
