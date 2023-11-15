74c73962afb247b566c011221a93485cc3982b0c
Namnika <janbandhunamnika@gmail.com>
2023-11-15 15:52:45 +0530

    removed some countries from countries array

--- src/pages/PopularNews.js
+++ src/pages/PopularNews.js
@@ -7,33 +7,23 @@ export default function PopularNews() {
 	const apiKey = "3d993edcc0e34e28b84450d9f7c95e36";
 
 	const countries = [
-		"ae",
 		"ar",
 		"it",
 		"jp",
 		"kr",
 		"ch",
 		"au",
-		"cn",
-		"co",
 		"de",
 		"ca",
 		"eg",
 		"fr",
 		"gb",
-		"gr",
-		"tr",
-		"tw",
 		"ua",
 		"us",
-		"ve",
 		"za",
 		"mx",
 		"my",
-		"ng",
-		"sa",
-		"se",
-		"sg"
+		"ng"
 	];
 
 	const country = Math.floor(
@@ -48,6 +38,7 @@ export default function PopularNews() {
 	);
 
 	const popularNews = news.slice(0, 3);
+	console.log(popularNews)
 
 	return (
 		<>
@@ -59,25 +50,35 @@ export default function PopularNews() {
 						return (
 							<>
 								<div className="flex">
-									<div>
+									<div key={index}>
 										<img
-											className="lg:w-[150px] lg:h-[170px] w-[150px] h-[170px]"
-											src={i.urlToImage}
-											width={150}
-											height={200}
+											key={index}
+											className=" lg:h-[170px] h-[170px]"
+											src={i.urlToImage != null && i.urlToImage}
+											width={250}
+											height={300}
 											alt="img"
 										/>
 									</div>
-									<div className="text-left space-x-5 space-y-5 antialiased leading-8">
+									<div
+
+										className="text-left space-x-5 space-y-5 antialiased leading-8"
+									>
 										<h3 className="text-4xl text-[color:hsl(233,8%,79%)] ml-5 font-['Inter-Bold']">
 											0{index + 1}
 										</h3>
 
-										<h4 className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] cursor-pointer hover:text-[color:hsl(5,85%,63%)]">
-											{`${i.title.slice(0, 25)}...`}
+										<h4
+											key={index}
+											className="text-lg font-['Inter-Bold'] text-[color:hsl(240,100%,5%)] cursor-pointer hover:text-[color:hsl(5,85%,63%)]"
+										>
+											{`${i.title.slice(0, 30)}...`}
 										</h4>
 
-										<p className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2 md:line-clamp-2">
+										<p
+											key={index}
+											className="text-[15px] font-['Inter-Regular'] text-[color:hsl(236,13%,42%)] line-clamp-2 md:line-clamp-2"
+										>
 											{i.description}
 										</p>
 									</div>
