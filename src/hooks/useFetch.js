300b24805ec7cc218c0907ff4c38549ea4066706
Namnika <janbandhunamnika@gmail.com>
2023-11-29 18:56:58 +0530

    removed ref param, setup `setter func` for all news

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,32 +1,38 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useFetch = (url, ref, initialvalue) => {
+export const useFetch = (url, initialvalue) => {
     const [news, setNews] = useState(initialvalue);
+    const [popularNews, setPopularNews] = useState(initialvalue);
+    const [trendingNews, setTrendingNews] = useState(initialvalue);
     const [error, setError] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        if (ref.current) {
-            (async () => {
-                const timeout = (ms) =>
-                    new Promise((resolve) => setTimeout(resolve, ms));
-                try {
-                    const res = await axios.get(url);
-                    setNews(res.data.articles);
-                } catch (err) {
-                    setError(err.response.data);
-                } finally {
-                    await timeout(5000);
-                    setLoading(false);
-                }
-            })();
-        }
+        (async () => {
+            const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+            try {
+                const res = await axios.get(url);
+                setNews(res.data.articles);
+                setPopularNews(res.data.articles);
+                setTrendingNews(res.data.articles);
+            } catch (err) {
+                setError(err.response.data);
+            } finally {
+                await timeout(5000);
+                setLoading(false);
+            }
+        })();
+    }, [url]);
 
-        return () => {
-            ref.current = false;
-        };
-    }, [url, ref]);
-
-    return { loading, news, error };
+    return {
+        loading,
+        news,
+        error,
+        popularNews,
+        trendingNews,
+        setNews,
+        setPopularNews,
+        setTrendingNews
+    };
 };
