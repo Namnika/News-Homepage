709169f27bb25a656e884cb3dbbd93004cffc97e
Namnika <janbandhunamnika@gmail.com>
2023-11-29 18:54:29 +0530

    created all varieties of news data

---
+++ src/utils/NewsTopics.js
@@ -0,0 +1,45 @@
+// all news varieties
+
+export const countries = [
+    "ar",
+    "it",
+    "jp",
+    "kr",
+    "ch",
+    "au",
+    "de",
+    "ca",
+    "eg",
+    "fr",
+    "gb",
+    "ua",
+    "us",
+    "za",
+    "mx",
+    "my",
+    "ng"
+];
+
+export const topics = [
+    "bitcoin",
+    "apple",
+    "trump",
+    "google",
+    "microsoft",
+    "amazon",
+    "world",
+    "artificial-intelligence",
+    "netflix",
+    "art"
+];
+
+export const sources = [
+    "bbc-news",
+    "abc-news",
+    "forbes",
+    "business-insider",
+    "washington-post",
+    "the-daily-times",
+    "cnn",
+    "billboard"
+];
