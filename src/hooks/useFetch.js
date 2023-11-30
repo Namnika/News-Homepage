16416d38f47d8f030c672a424465bbd21e3d67c2
Namnika <janbandhunamnika@gmail.com>
2023-11-30 20:11:26 +0530

    added options to axios

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,38 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useFetch = (url, initialvalue) => {
+export const useFetch = ({ url, options }, ref, initialvalue) => {
     const [news, setNews] = useState(initialvalue);
-    const [popularNews, setPopularNews] = useState(initialvalue);
-    const [trendingNews, setTrendingNews] = useState(initialvalue);
     const [error, setError] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        (async () => {
-            const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-            try {
-                const res = await axios.get(url);
-                setNews(res.data.articles);
-                setPopularNews(res.data.articles);
-                setTrendingNews(res.data.articles);
-            } catch (err) {
-                setError(err.response.data);
-            } finally {
-                await timeout(5000);
-                setLoading(false);
-            }
-        })();
-    }, [url]);
+        setLoading(true);
+        if (ref.current) {
+            (async () => {
+                const timeout = (ms) => new Promise((resolve) => { setTimeout(resolve, ms) })
+                try {
+                    const res = await axios.get(url, options);
+                    setNews(res.data.articles);
+                } catch (err) {
+                    setError(err.response.data);
+                } finally {
+                    await timeout(7000)
+                    setLoading(false);
+                }
+            })();
+        }
+
+        return () => {
+            ref.current = false
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [url, ref]);
 
     return {
         loading,
         news,
         error,
-        popularNews,
-        trendingNews,
         setNews,
-        setPopularNews,
-        setTrendingNews
     };
 };
