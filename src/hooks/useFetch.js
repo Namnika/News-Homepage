d8f2aaff16557eec72ae0ec606ac273986abb970
Namnika <janbandhunamnika@gmail.com>
2023-12-12 20:23:32 +0530

    modified `get` request to `request` config options

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,39 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useFetch = ({ url, options }, ref, initialvalue) => {
-    const [news, setNews] = useState(initialvalue);
-    const [error, setError] = useState({});
-    const [loading, setLoading] = useState(true);
+export const useFetch = (options, ref, initialvalue) => {
+	const [news, setNews] = useState(initialvalue);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true);
-        if (ref.current) {
-            (async () => {
-                const timeout = (ms) => new Promise((resolve) => { setTimeout(resolve, ms) })
-                try {
-                    const res = await axios.get(url, options);
-                    setNews(res.data.articles);
-                } catch (err) {
-                    setError(err.response.data);
-                } finally {
-                    await timeout(7000)
-                    setLoading(false);
-                }
-            })();
-        }
+	useEffect(() => {
+		setLoading(true);
+		if (ref.current) {
+			const sleep = (s) =>
+				new Promise((resolve) => setTimeout(resolve, s * 1000));
+			(async () => {
+				try {
+					await sleep(10);
+					const res = await axios.request(options);
+					setNews(res.data.articles);
+				} catch (err) {
+					setError(err.message);
+				} finally {
+					await sleep(30);
+					setLoading(false);
+				}
+			})();
+		}
 
-        return () => {
-            ref.current = false
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [url, ref]);
+		return () => {
+			ref.current = false;
+		};
+	}, [options, ref]);
 
-    return {
-        loading,
-        news,
-        error,
-        setNews,
-    };
+	return {
+		loading,
+		news,
+		error,
+		setNews
+	};
 };
