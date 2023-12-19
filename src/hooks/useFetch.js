91ac74e29e5cd82ffa568dce8756b2c87fd303a4
Namnika <janbandhunamnika@gmail.com>
2023-12-19 17:56:26 +0530

    removed ref property

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,34 +1,23 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useFetch = (options, ref, initialvalue) => {
+export const useFetch = (options, initialvalue) => {
 	const [news, setNews] = useState(initialvalue);
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		setLoading(true);
-		if (ref.current) {
-			const sleep = (s) =>
-				new Promise((resolve) => setTimeout(resolve, s * 1000));
-			(async () => {
-				try {
-					await sleep(10);
-					const res = await axios.request(options);
-					setNews(res.data.articles);
-				} catch (err) {
-					setError(err.message);
-				} finally {
-					await sleep(30);
-					setLoading(false);
-				}
-			})();
-		}
-
-		return () => {
-			ref.current = false;
-		};
-	}, [options, ref]);
+		(async () => {
+			try {
+				const res = await axios.request(options);
+				setNews(res.data.articles);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, [options]);
 
 	return {
 		loading,
