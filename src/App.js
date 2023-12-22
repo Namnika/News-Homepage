d2c9d812ba7867fd3cac29097d057863197416c1
Namnika <janbandhunamnika@gmail.com>
2023-12-22 18:59:13 +0530

    add no match route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Main from "./pages/Main";
+import News from "./pages/News";
+import PopularNews from "./pages/PopularNews"
+import TrendingNews from "./pages/TrendingNews";
+import NoMatch from "./pages/NoMatch";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css"
 
@@ -7,6 +11,10 @@ function App() {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Main />} />
+				<Route path="#news" element={<News />} />
+				<Route path="#popular" element={<PopularNews />} />
+				<Route path="#trending" element={<TrendingNews />} />
+				<Route path="*" element={<NoMatch />} />
 			</Routes>
 		</Router>
 	);
