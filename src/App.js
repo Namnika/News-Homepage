1363ae7e4090892bb1a4f9167d42d004b4961b27
Namnika <janbandhunamnika@gmail.com>
2023-12-24 22:36:29 +0530

    removed `<Router>`

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import Main from "./pages/Main";
 import News from "./pages/News";
-import PopularNews from "./pages/PopularNews"
+import PopularNews from "./pages/PopularNews";
 import TrendingNews from "./pages/TrendingNews";
 import NoMatch from "./pages/NoMatch";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/index.css"
+import { Routes, Route } from "react-router-dom";
+import "./styles/index.css";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route path="#news" element={<News />} />
-				<Route path="#popular" element={<PopularNews />} />
-				<Route path="#trending" element={<TrendingNews />} />
-				<Route path="*" element={<NoMatch />} />
-			</Routes>
-		</Router>
+		<Routes>
+			<Route path="/" element={<Main />} />
+			<Route path="#news" element={<News />} />
+			<Route path="#popular" element={<PopularNews />} />
+			<Route path="#trending" element={<TrendingNews />} />
+			<Route path="*" element={<NoMatch />} />
+		</Routes>
 	);
 }
 export default App;
